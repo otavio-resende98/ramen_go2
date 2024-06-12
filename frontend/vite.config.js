f9fbeb23c7054be664f0b2664780063274d6f458
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 18:22:11 -0300

    Teste https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace('/api', '')
       }
