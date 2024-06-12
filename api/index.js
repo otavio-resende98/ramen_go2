d6af06705ee7628e4c93eb52731152e579b87f9a
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 14:55:20 -0300

    Configuração de variáveis vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 //Middleware
 app.use(bodyParser.json());
@@ -18,5 +32,6 @@ app.use('/proteins', proteins);
 const orders = require('./routes/orders');
 app.use('/orders', orders);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log(`Servidor iniciado na porta ${port}`)})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
