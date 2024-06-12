0d4f529a450c6c81cf8b5e32f6adbb0b5dc6dc2c
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 15:28:57 -0300

    Vercel configurado

--- client/main.js
+++ client/main.js
@@ -1,81 +1,24 @@
-import './styles.css';
-import  {listBroths}  from './src/components/broths.js';
-import {listProteins} from './src/components/proteins.js';
-import {scrollWidgetBroth} from './src/components/widgetbroth.js';
-import {scrollWidgetProtein} from './src/components/widgetprotein.js';
-import {buttonPlaceNewOrder} from './src/components/buttonOrder.js';
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <body>    
-    <section>
-      <div id="container_principal">
-          <div class="logo">
-            <img src="./public/images/logo.png">
-          </div>
-          <div id="container_capa">
-            <div class="circulo-marrom">
-              <div class="entregadora">
-                <img src="./public/images/entregadora.png">
-              </div>
-            </div>
-            <div class="balaoAzul">
-              <img src="./public/images/BalaoAzul.png">
-            </div>
-            <div class="balaoAmarelo">
-              <img src="./public/images/BalaoAmarelo.png">
-            </div>
-          </div>
-          <div class="letras-amarelas"> ラーメン </div>
-          <div class="title-go">GO!</div>
-          <div class="description"></div>
-          <div class="button-blue"></div>
-      </div>
-    </section>
-    <section>
-      <div id="container_body">
-        <section>
-          <div id="title_select_broth">
-            First things first: select your favorite broth.
-          </div>
-          <div id="subtitle_select_broth">
-            It will give the whole flavor on your ramen soup. 
-          </div>
-        </section>
-        <section>
-          <div id="container_select_broth"></div>
-        </section>
-        <section>
-          <div class="container-widget">
-            <div id="select-broth-widget-container"></div>
-          </div>
-        </section>
-        <section>
-          <div id="title_select_broth">
-            It’s time to choose (or not) your meat!
-          </div>
-          <div id="subtitle_select_broth">
-            Some people love, some don’t. We have options for all tastes. 
-          </div>
-        </section>
-        <section>
-          <div id="container_select_protein"></div>
-        </section>
-        <section>
-          <div class="container-widget">
-            <div id="select-protein-widget-container"></div>
-          </div>
-        </section>
-        <section>
-          <div id="button_new_order_sucess"></div>
-        </section>
-      </div>
-    </section>
-  </body>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
 `
 
-listBroths(document.querySelector('#container_select_broth'));
-listProteins(document.querySelector('#container_select_protein'));
-scrollWidgetBroth(document.querySelector('#select-broth-widget-container'));
-scrollWidgetProtein(document.querySelector('#select-protein-widget-container'));
-buttonPlaceNewOrder(document.querySelector('#button_new_order_sucess'))
-
+setupCounter(document.querySelector('#counter'))
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 3000, // Porta do servidor de desenvolvimento
+  },
   build: {
-    outDir: 'dist',  // Garante que a saída seja para a pasta 'dist'
-    emptyOutDir: true,  // Limpa o diretório de saída antes de construir
-    rollupOptions: {
-      input: {
-        main: './main.js',  // Arquivo de entrada principal
-      }
-    }
+    outDir: 'dist'  // Garante que a saída seja para a pasta 'dist'
   }
 });
