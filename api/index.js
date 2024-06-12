01f24d2bbf8ec385c021bda61f9144c2f11d583e
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 15:29:27 -0300

    Configuração vercel

--- api/index.js
+++ api/index.js
@@ -5,21 +5,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+// Conectar ao MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Failed to connect to MongoDB', err);
-});
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
 
+// Definir uma rota de teste
 app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello, World!' });
+  res.json({ message: 'Hello, World!' });
 });
 
+// Iniciar o servidor
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
