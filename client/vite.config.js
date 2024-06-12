b338651b99ced3b431cda770ee4ddec140f08fdd
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 15:11:07 -0300

    Teste vercel 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue()],
   build: {
-    outDir: 'dist'  // Garante que a saída seja para a pasta 'dist'
-  }
+    outDir: 'dist',  // Garante que a saída seja para a pasta 'dist'
+  },
 });
