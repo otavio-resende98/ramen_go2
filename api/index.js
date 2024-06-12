0c87e6b155eb506683601ac58ebb4dbb6888b995
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 16:33:35 -0300

    Arquivos faltando

---
+++ api/index.js
@@ -0,0 +1,36 @@
+// api/main.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = 'mongodb+srv://root:root@db-express.4fwcimy.mongodb.net/?retryWrites=true&w=majority&appName=DB-Express';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Conectado ao MongoDB');
+});
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routers
+const broths = require('./routes/broths');
+app.use('/broths', broths);
+
+const proteins = require('./routes/proteins');
+app.use('/proteins', proteins);
+
+const orders = require('./routes/orders');
+app.use('/orders', orders);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
