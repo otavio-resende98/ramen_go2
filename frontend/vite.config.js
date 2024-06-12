ce6eb8e6976e73889d34f3d1f23a79bf2a620417
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 18:32:05 -0300

    Teste

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    outDir: '../public', // Diretório de saída para os arquivos de build
-    emptyOutDir: true, // Limpar o diretório de saída antes de construir
-  }
-});
+export default defineConfig({});
