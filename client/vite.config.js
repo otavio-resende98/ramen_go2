5a115ee4a40b32b808d582655709d3dfc5b684fe
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 16:01:52 -0300

    Teste vercel final

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',  // Garante que a saída seja para a pasta 'dist'
-    emptyOutDir: true,  // Limpa o diretório de saída antes de construir
-    rollupOptions: {
-      input: {
-        main: './main.js',  // Arquivo de entrada principal
-      }
-    }
-  }
-});
+  plugins: [vue()],
+})
