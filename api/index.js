6910e59729adde0d5bba313d759d7eb2507aba1c
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 15:08:03 -0300

    Teste vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send({ message: 'Hello, World!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+/*const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -34,4 +59,4 @@ app.use('/orders', orders);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+});*/
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  transpileDependencies: true,
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://localhost:3000'
-      }
-    }
-  },
   build: {
     outDir: 'dist'  // Garante que a saída seja para a pasta 'dist'
   }
-})
+});
