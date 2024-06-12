13abc89ebdf8e6d84fa3411d7ea8b2f57cccc43b
Otávio Resende <otavio.augusto@geotech4web.com.br>
2024-06-12 15:18:39 -0300

    Teste 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
   build: {
     outDir: 'dist',  // Garante que a saída seja para a pasta 'dist'
-  },
+    emptyOutDir: true,  // Limpa o diretório de saída antes de construir
+    rollupOptions: {
+      input: {
+        main: './src/main.js',  // Arquivo de entrada principal
+      }
+    }
+  }
 });
